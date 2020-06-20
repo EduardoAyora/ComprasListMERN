4f2f5448f16e743e020fbe52e030934916241bb4
EduardoAyora <eduardoaayora64@gmail.com>
2020-06-19 21:34:50 -0500

    ya te logueasss

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     'secretKey': 'conEstClvenoPuedenDescNunca',
-    'mongoUrl' : 'mongodb://localhost:27017/compras'
+    // 'mongoUrl' : 'mongodb://localhost:27017/compras'
+    'mongoUrl' : 'mongodb+srv://eduardo:9T2WhWWlSdWY5rDX@cluster0.qkrxw.mongodb.net/compras?retryWrites=true&w=majority'
 }

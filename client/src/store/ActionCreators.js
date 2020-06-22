543be657cfd2071e9b04ab1ac0b65e7c66dd578e
EduardoAyora <eduardoaayora64@gmail.com>
2020-06-21 21:45:53 -0500

    ya funca login

--- client/src/store/ActionCreators.js
+++ client/src/store/ActionCreators.js
@@ -243,8 +243,36 @@ export const loginUser = (creds) => (dispatch) => {
             error.response = response;
             throw error;
         }
+
+        dispatch(productsLoading(true));
+        const bearer = 'Bearer ' + localStorage.getItem('token');
+        return fetch('/products', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': bearer
+            },
+            credentials: 'same-origin'
+        })
+
     })
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
+    .then(response => response.json())
+    .then(products => dispatch(addProducts(products)))
     .catch(error => dispatch(loginError(error.message)))
+    .catch(error => dispatch(productsFailed(error.message)));
 };
 
 export const requestLogout = () => {

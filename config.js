0a022f034ecbb46a95ed386ff7596fe3465c7b5d
EduardoAyora <eduardoaayora64@gmail.com>
2020-07-07 20:39:27 -0500

    mensajes de error o exito ya se quitan del login

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secretKey': 'conEstClvenoPuedenDescNunca',
-    'mongoUrl' : 'mongodb://localhost:27017/compras'
-    // 'mongoUrl' : 'mongodb+srv://eduardo:9T2WhWWlSdWY5rDX@cluster0.qkrxw.mongodb.net/compras?retryWrites=true&w=majority'
+    // 'mongoUrl' : 'mongodb://localhost:27017/compras'
+    'mongoUrl' : 'mongodb+srv://eduardo:9T2WhWWlSdWY5rDX@cluster0.qkrxw.mongodb.net/compras?retryWrites=true&w=majority'
 }
